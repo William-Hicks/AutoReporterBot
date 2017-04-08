4c929f2f2832a5896092e6988c5ad22db02a351d
William Hicks <w.hicks917@gmail.com>
2017-04-08 15:35:37 +0000

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,32 @@
+sub_listing = ['LifeProTips',"PeopleFuckingDying","BiggerThanYouThought",'Celebs','Starlets','KateUpton','GentlemanBoners','EmmaWatson','WatchItForThePlot','WhatsWrongWithYourDog','food']
+
+
+responses = [
+	'I am a bot',
+	'This action was performed automatically'	
+]
+master_dict = {
+	'group_one': {
+		'starlets': {'report':2, 'remove': 4, 'verbose':True}, 
+		'Celebs': {'report':2,'remove': 4, 'verbose':True}, 
+		'GentlemanBoners': {'report':2, 'remove':5, 'verbose':True},
+		'JenniferLawrence':{'report':2,'remove':4,'verbose':True},
+		'lastimages':{'report':2,'remove':3,'verbose':True}},
+	'group_two': {
+		'PeopleFuckingDying':{'report':2,'remove':3, 'verbose':False}, 
+		'EmmaWatson': {'report':2,'remove':3,'verbose':False},
+		'AlexisRen': {'report':2,'remove':3,'verbose':False},
+		'katyperry':{'report':2,'remove':3,'verbose':False}},
+	'group_three': {
+		'food': {'report':3,'remove':5, 'verbose':False},
+		'LifeProTips': {'report':3,'remove':5,'verbose':False}}, 
+	'group_four': { 
+		'kateupton': {'report':3,'remove':4, 'verbose':False}, 
+		'WatchItForThePlot':{'report':2,'remove':4,'verbose':True}, 
+		'WhatsWrongWithYourDog':{'report':2,'remove':3, 'verbose':True}, 
+		'CanineCosplay': {'report':2,'remove':3,'verbose':False},
+		'surpriseddogs': {'report':2,'remove':3, 'verbose':False},
+		'CanineMajesty':{'report':2,'remove':3,'verbose': False},
+		'NatalieDormer':{'report':2,'remove':4,'verbose':False},
+		'shorthairedhotties': {'report':2,'remove':3, 'verbose':False}}
+}
